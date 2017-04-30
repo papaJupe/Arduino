7a1c7d2cab64d96ff04a1602e4801dbc60700379
papaJupe <malexead@yahoo.com>
2017-04-30 12:06:53 -0600

    add active beeper option
    use with passive or active

--- TonerTest/TonerTest.ino
+++ TonerTest/TonerTest.ino
@@ -1,18 +1,26 @@
-void setup() {
-  // put your setup code here, to run once:
-
+void setup() 
+{
+  // setup code run once:
+pinMode(8, OUTPUT);
 }
 
-void loop() {
+void loop() 
   // put your main code here, to run repeatedly:
  {
       for (byte i = 0; i <= 4; i++) // beep tone every 20 sec
-      {
-        tone(8, 2800);   // don't need pin config for toner
+//      { // passive beeper uses this
+//        tone(8, 2800);   // don't need pin config for toner
+//        delay(500);
+//        noTone(8);
+//        delay(200);
+//      } // end tone loop
+
+      {  // active piezo beeper just needs voltage
+        digitalWrite(8, HIGH);   //  need pin config for digiWrite to work
         delay(500);
-        noTone(8);
+        digitalWrite(8, LOW);
         delay(200);
       } // end tone loop
-      delay (5000);   // 2 + 18 = 20 sec between plays
-    }
-}
+      delay (3000); 
+    
+} // end loop
